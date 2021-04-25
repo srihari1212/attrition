7176339ec4189831fb113041cd42b246763af1b2
srihari1212 <45137382+srihari1212@users.noreply.github.com>
2021-04-25 13:07:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -88,12 +88,38 @@ def attrition():
         put_text('  ')
         put_text('  ')
         put_text('when attrition crosses a particular threshold, it becomes a cause for concern. For example, attrition among minority employee groups could be hurting diversity at your organization. Or, attrition among senior leaders can lead to a significant gap in organizational leadership')
+        put_text('  ')
         put_text('Here are some steps to reduce attrition rate!')
-        put_text('  ')
-        put_text('1.  Communicate your vision properly. When your staff is in the loop of what’s driving the business, they will share in the same vision that you have. It earns their dedication and commitment')
-        put_text('  ')
-        put_text('2.  Communicate your vision properly. When your staff is in the loop of what’s driving the business, they will share in the same vision that you have. It earns their dedication and commitment')
         
+        put_text('1.  COMMUNICATE YOUR VISION')
+        put_text('      When your staff is in the loop of what’s driving the business, they will share in the same vision that you have. It earns their dedication and commitment')
+        put_text('  ')
+        
+        put_text('2.  OPTIMIZE RECRUITMENT')
+        put_text('      You can optimize your recruitment process by starting with clear and specific requirements. Set goals for hiring for a position and clearly list the tasks and responsibilities, and what value the position will bring to your business')
+        put_text('  ')
+        
+        put_text('3.  MAKE THE INTERVIEW MATTER')
+        put_text('      The interview questions should be based on past and present work performance and behaviours. Allow the candidate to demonstrate their skill levels, motivations and competencies in their fields of experience')
+        put_text('  ')
+        
+        put_text('4.  IMPROVE WORK CONDITIONS')
+        put_text('      What you offer as work benefits is a big deal for your employees. Top companies that are known for their perks for their employees have strong development programs, outstanding benefits not only for employees but also to their families, and fun work cultures. When a business knows to meet the needs of their employees beyond the office, they benefit more from their employees.')
+        put_text('  ')
+        
+        put_text('5.  CREATE A PLEASANT WORKSPACE')
+        put_text('      Employees spend almost half a day inside their workplaces. Any person would want that place to be where they are most productive, happy, healthy, and engaged. A person’s well-being affects his productivity and work performance, so it is common sense to provide for such.')
+        put_text('  ')
+        
+        put_text('6.  BENEFITS AND PERKS')
+        put_text('      The most common reason employees leave is because of the their salary. No matter how loyal and how driven they are with the company’s vision, if it cannot meet with their financial needs, they often look for new jobs.A great addition to any salary package are the benefits. You can add in paid time off, stock options, and even education assistance. ')
+        put_text('  ')
+        
+        put_text('7.  EMPLOYEE ENAGEMENT')
+        put_text('      When you have talented employees, you need to find ways that you can help them expand their skill set. Give your feedback, let them know what you think. Pay attention, and let them know that you are there for them. If you don’t engage with them, they will get bored and complacent, and think that they are not growing within the organization.')
+        put_text('  ')
+        
+    
     else:
         put_text('Employee will stay!')
         put_image(open('stay.jpg', 'rb').read())
